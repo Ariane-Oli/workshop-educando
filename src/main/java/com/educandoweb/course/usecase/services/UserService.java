424c1242cce9bdc94236305228d5dcf5c2de9721
Ariane Oliveira <ariane.de@outlook.com>
2025-11-01 06:21:00 -0300

    Adding new class UserService

---
+++ src/main/java/com/educandoweb/course/usecase/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.usecase.services;
+
+import com.educandoweb.course.adapters.ports.output.UserRepository;
+import com.educandoweb.course.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
